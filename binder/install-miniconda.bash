8e7b7c1c60c347ace7169893939bd49a028cfa21
Thomas Erben <terben@astro.uni-bonn.de>
2019-08-07 22:26:11 +0200

    added binder files for binderhub

---
+++ binder/install-miniconda.bash
@@ -0,0 +1,79 @@
+#!/bin/bash
+# This downloads and installs a pinned version of miniconda
+set -ex
+
+cd $(dirname $0)
+MINICONDA_VERSION=4.6.14
+CONDA_VERSION=4.6.14
+# Only MD5 checksums are available for miniconda
+# Can be obtained from https://repo.continuum.io/miniconda/
+MD5SUM="718259965f234088d785cad1fbd7de03"
+
+URL="https://repo.continuum.io/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh"
+INSTALLER_PATH=/tmp/miniconda-installer.sh
+
+# make sure we don't do anything funky with user's $HOME
+# since this is run as root
+unset HOME
+
+wget --quiet $URL -O ${INSTALLER_PATH}
+chmod +x ${INSTALLER_PATH}
+
+# check md5 checksum
+if ! echo "${MD5SUM}  ${INSTALLER_PATH}" | md5sum  --quiet -c -; then
+    echo "md5sum mismatch for ${INSTALLER_PATH}, exiting!"
+    exit 1
+fi
+
+bash ${INSTALLER_PATH} -b -p ${CONDA_DIR}
+export PATH="${CONDA_DIR}/bin:$PATH"
+
+# Allow easy direct installs from conda forge
+conda config --system --add channels conda-forge
+
+# Do not attempt to auto update conda or dependencies
+conda config --system --set auto_update_conda false
+conda config --system --set show_channel_urls true
+
+# bug in conda 4.3.>15 prevents --set update_dependencies
+echo 'update_dependencies: false' >> ${CONDA_DIR}/.condarc
+
+# install conda itself
+if [[ "${CONDA_VERSION}" != "${MINICONDA_VERSION}" ]]; then
+    conda install -yq conda==${CONDA_VERSION}
+fi
+
+echo "installing notebook env:"
+cat /tmp/environment.yml
+conda env create -p ${NB_PYTHON_PREFIX} -f /tmp/environment.yml
+
+# empty conda history file,
+# which seems to result in some effective pinning of packages in the initial env,
+# which we don't intend.
+# this file must not be *removed*, however
+echo '' > ${NB_PYTHON_PREFIX}/conda-meta/history
+
+# enable nteract-on-jupyter, which was installed with pip
+jupyter serverextension enable nteract_on_jupyter --sys-prefix
+
+if [[ -f /tmp/kernel-environment.yml ]]; then
+    # install kernel env and register kernelspec
+    echo "installing kernel env:"
+    cat /tmp/kernel-environment.yml
+
+    conda env create -p ${KERNEL_PYTHON_PREFIX} -f /tmp/kernel-environment.yml
+    ${KERNEL_PYTHON_PREFIX}/bin/ipython kernel install --prefix "${NB_PYTHON_PREFIX}"
+    echo '' > ${KERNEL_PYTHON_PREFIX}/conda-meta/history
+    conda list -p ${KERNEL_PYTHON_PREFIX}
+fi
+
+# Clean things out!
+conda clean -tipsy
+
+# Remove the big installer so we don't increase docker image size too much
+rm ${INSTALLER_PATH}
+
+chown -R $NB_USER:$NB_USER ${CONDA_DIR}
+
+conda list -n root
+conda list -p ${NB_PYTHON_PREFIX}
